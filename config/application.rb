1d5f10eed309002bfe1c6029b650f1dc079e7db6
Quentin Burgniard <quentinburgniard@gmail.com>
2018-04-14 14:48:32 -0400

    add fonts MuseoSans regular|italic https://www.fontspring.com/fonts/exljbris/museo-sans?utm_source=fontsquirrel.com&utm_medium=download_link&utm_campaign=museo-sans#firstfreeproduct

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module GgFamily
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # https://gist.github.com/anotheruiguy/7379570
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
